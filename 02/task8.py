32f2357a27a97ec9749027e9990ad9e85e2b8061
Vlad <vlad.zah@ukr.net>
2025-01-20 20:16:17 +0200

    PC-2 Implemented task 8

---
+++ 02/task8.py
@@ -0,0 +1,18 @@
+def analyze_inheritance(class_name):
+    for parent_class_item in class_name.__bases__:
+        for item_name, item in vars(parent_class_item).items():
+            if callable(item):
+                print(f"- {item_name} from {parent_class_item.__name__}")
+
+
+class Parent:
+    def parent_method(self):
+        pass
+
+
+class Child(Parent):
+    def child_method(self):
+        pass
+
+
+analyze_inheritance(Child)
