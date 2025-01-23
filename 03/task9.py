4ba1eeb3d41511323f713d3c5349b56a5d435309
Vlad <vlad.zah@ukr.net>
2025-01-24 00:55:14 +0200

    PC-3 Implemented additional task

--- 03/task9.py
+++ 03/task9.py
@@ -110,16 +110,66 @@ class PriceDescriptor:
         """
         if value < 0:
             raise ValueError("Product price can't be less than 0")
-        instance.__dict__['_price'] = value
+        instance.__dict__['_price'] = round(value, 2)
+
+
+class CurrencyDescriptor:
+    """Class represents Currency changes and price exchange"""
+    EUR_TO_USD = 1.0418
+    USD_TO_EUR = 0.96
+
+    def __get__(self, instance: 'ProductWithDescriptor', owner) -> str:
+        """Get currency
+        :param instance:
+        :return str:
+        """
+        return instance.__dict__.get('currency')
+
+    def __set__(self, instance: 'ProductWithDescriptor', value: str) -> None:
+        """Set currency value and make changes in the price due to the currency exchange"""
+        self.__is_empty(value)
+        self.__is_currency_not_supported(value)
+
+        if 'currency' in instance.__dict__:
+            if instance.__dict__['currency'] == value.strip().upper():
+                return
+
+            if value.strip().upper() == "USD":
+                instance.__dict__['_price'] = round(instance.__dict__['_price'] * self.EUR_TO_USD, 2)
+            elif value.strip().upper() == "EUR":
+                instance.__dict__['_price'] = round(instance.__dict__['_price'] * self.USD_TO_EUR, 2)
+        instance.__dict__['currency'] = value.strip().upper()
+
+    def __is_empty(self, value: str) -> None:
+        """Throw ValueError if value is empty
+        :param value:
+        """
+        if value.strip() is None:
+            raise ValueError("Currency name can't be empty.")
+
+    def __is_currency_not_supported(self, value: str) -> None:
+        """
+        Throw ValueError if not supported currency provided
+        :param value:
+        :return:
+        """
+        if value.strip().upper() != "EUR" and value.strip().upper() != "USD":
+            raise ValueError(f"Currency {value} is not supported")
 
 
 class ProductWithDescriptor:
     """Represent product with price descriptor"""
     _price = PriceDescriptor()
+    currency = CurrencyDescriptor()
 
-    def __init__(self, name: str, price: float) -> None:
+    def __init__(self, name: str, price: float, currency: str) -> None:
         self.name = name
         self._price = price
+        self.currency = currency
+
+    def __repr__(self):
+        """Product into representation"""
+        return f"(price: {self._price}, currency: {self.currency})"
 
 
 ################## Unit tests
@@ -136,7 +186,7 @@ class ProductTest(unittest.TestCase):
         self.assertEqual(22, product_with_property.price)
 
         # For getting value in the unit tests property should be provided
-        product_with_descriptor = ProductWithDescriptor('Coffee', 22)
+        product_with_descriptor = ProductWithDescriptor('Coffee', 22, 'EUR')
         self.assertEqual(22, product_with_descriptor._price)
 
     def test_set_price_value(self):
@@ -150,13 +200,13 @@ class ProductTest(unittest.TestCase):
         self.assertEqual(4, product_with_property.price)
 
         # For setting value in the unit tests property should be provided
-        product_with_descriptor = ProductWithDescriptor('Coffee', 22)
+        product_with_descriptor = ProductWithDescriptor('Coffee', 22, 'EUR')
         product_with_descriptor._price = 23
         self.assertEqual(23, product_with_descriptor._price)
 
     def test_set_raise_value_error_in_product_with_descriptor(self):
         with self.assertRaises(ValueError):
-            product_with_descriptor = ProductWithDescriptor('Coffee', 22)
+            product_with_descriptor = ProductWithDescriptor('Coffee', 22, 'EUR')
             product_with_descriptor._price = -11
 
     def test_set_raise_value_error_in_product_with_property(self):
@@ -169,9 +219,6 @@ class ProductTest(unittest.TestCase):
             product_with_get_set = ProductWithGetSet('Coffee', 22)
             product_with_get_set.set_price(-22)
 
-
-if __name__ == '__main__':
-    unittest.main()
 
 ################### Programming call
 
@@ -189,9 +236,17 @@ try:
 except ValueError as error:
     print('product_with_property error:', error.__str__())
 
-product_with_descriptor = ProductWithDescriptor('Coffee', 26)
+product_with_descriptor = ProductWithDescriptor('Coffee', 22, 'EUR')
 print('product_with_descriptor:', product_with_descriptor._price)
 try:
     product_with_descriptor._price = -22
 except ValueError as error:
     print('product_with_descriptor error:', error.__str__())
+
+print('---- Currency Exchange ----')
+
+print(product_with_descriptor)
+product_with_descriptor.currency = 'USD'
+print(product_with_descriptor)
+product_with_descriptor.currency = 'EUR'
+print(product_with_descriptor)
