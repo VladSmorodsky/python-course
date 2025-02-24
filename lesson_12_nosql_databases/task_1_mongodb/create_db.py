86e5948cec942390c6ca5d00a3d622eb506c473a
Vlad <vlad.zah@ukr.net>
2025-02-24 09:58:29 +0200

    PC-12 Added index for products' category

--- lesson_12_nosql_databases/task_1_mongodb/create_db.py
+++ lesson_12_nosql_databases/task_1_mongodb/create_db.py
@@ -1,7 +1,7 @@
 import os
 from typing import Mapping, Any
 
-from pymongo import MongoClient
+from pymongo import MongoClient, TEXT
 from dotenv import load_dotenv
 from pymongo.synchronous.collection import Collection
 
@@ -9,6 +9,11 @@ load_dotenv()
 
 client = MongoClient(os.getenv("DATABASE_CONNECTION"))
 db = client[os.getenv("DATABASE_NAME")]
+product_collection = db['products']
+order_collection = db['orders']
+
+# Create index for category field for faster search result
+product_collection.create_index([("category", TEXT)])
 
 
 def get_product_collection() -> Collection[Mapping[str, Any] | Any]:
@@ -16,7 +21,6 @@ def get_product_collection() -> Collection[Mapping[str, Any] | Any]:
     Get a product collection.
     :return:
     """
-    product_collection = db['products']
     return product_collection
 
 
@@ -25,7 +29,6 @@ def get_order_collection() -> Collection[Mapping[str, Any] | Any]:
     Get an order collection.
     :return:
     """
-    order_collection = db['orders']
     return order_collection
 
 
