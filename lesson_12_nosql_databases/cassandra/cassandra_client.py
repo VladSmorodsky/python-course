d84e793db019a99a1061e2eebc01bd19d7508c6d
Vlad <vlad.zah@ukr.net>
2025-02-27 15:41:47 +0200

    PC-12 Added context manager, adjusted update and delete queries, added other table

--- lesson_12_nosql_databases/cassandra/cassandra_client.py
+++ lesson_12_nosql_databases/cassandra/cassandra_client.py
@@ -13,99 +13,173 @@ in order.
 This strategy allows control over the distribution of replicas across zones,
 ensuring high availability and fault tolerance.
 """
-
-
+import os
+import traceback
 import uuid
+from contextlib import contextmanager
 from datetime import datetime, timedelta
-from typing import Any, Optional
+from typing import Any, Optional, Generator
 
-from cassandra.cluster import Cluster
+from cassandra.cluster import Cluster, ConnectionException, NoHostAvailable, Session
+from cassandra import InvalidRequest
+from cassandra.protocol import SyntaxException
+from cassandra.query import BatchStatement
+from dotenv import load_dotenv
 
 from customer import Customer
 
-cluster = Cluster(['localhost'])
-session = cluster.connect()
+load_dotenv()
+
+cluster_name = os.getenv("CASSANDRA_CLUSTER_NAME", 'localhost')
+cluster_port = os.getenv("CASSANDRA_CLUSTER_PORT", 9042)
 
 
-def create_event_logs_table() -> None:
+@contextmanager
+def cassandra_session() -> Generator[Session, None, None]:
+    """
+    Create a Cassandra session.
+    :return:
+    """
+    cluster = Cluster([cluster_name], port=cluster_port)
+    session = cluster.connect()
+    try:
+        yield session
+    except ConnectionException | NoHostAvailable | SyntaxException as error:
+        print(f"Cluster not available: {error}")
+    finally:
+        session.shutdown()
+        cluster.shutdown()
+
+
+def create_event_logs_table(session: Session) -> None:
     """
     Create event_logs table
+    :param session:
     :return:
     """
     # Create key space (db analog)
-    session.execute("""
-    CREATE KEYSPACE IF NOT EXISTS logger
-    WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'replication_factor' : 3 }
-    """)
+    try:
+        session.execute("""
+        CREATE KEYSPACE IF NOT EXISTS logger
+        WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'dc1': 3, 'dc2': 3 }
+        """)
 
-    session.set_keyspace('logger')
-    session.execute("""
-    CREATE TABLE IF NOT EXISTS event_logs (
-        event_id UUID,
-        user_id UUID,
-        event_type TEXT,
-        timestamp TIMESTAMP,
-        metadata MAP<TEXT, TEXT>,
-        PRIMARY KEY (event_id, timestamp)
-        )
-    """)
+        session.set_keyspace('logger')
+        session.execute("""
+        CREATE TABLE IF NOT EXISTS event_logs_by_type (
+            event_id UUID,
+            user_id UUID,
+            event_type TEXT,
+            created_at TIMESTAMP,
+            metadata MAP<TEXT, TEXT>,
+            PRIMARY KEY ((event_type), created_at)
+            )
+        """)
+        session.execute("""
+        CREATE TABLE IF NOT EXISTS event_logs (
+            event_id UUID,
+            user_id UUID,
+            event_type TEXT,
+            created_at TIMESTAMP,
+            metadata MAP<TEXT, TEXT>,
+            PRIMARY KEY (event_id)
+            )
+        """)
+    except InvalidRequest as error:
+        print(error)
 
 
-def insert_event(customer: Customer, event_type: str, metadata: Optional[dict[str, Any]] = None,
+def insert_event(session: Session, customer: Customer, event_type: str, metadata: Optional[dict[str, Any]] = None,
                  timestamp: datetime = None) -> None:
     """
     Insert an event into the database.
+    :param session:
+    :param customer:
+    :param event_type:
+    :param metadata:
+    :param timestamp:
     :return:
     """
-    event_id = uuid.uuid4()
-    timestamp = timestamp if timestamp is not None else datetime.now()
+    try:
+        event_id = uuid.uuid4()
+        timestamp = timestamp if timestamp is not None else datetime.now()
 
-    session.execute("""
-    INSERT INTO event_logs (event_id, user_id, event_type, timestamp, metadata)
-    VALUES (%s, %s, %s, %s, %s)
-    """, (event_id, customer.id, event_type, timestamp, metadata))
+        session.execute("""
+        INSERT INTO event_logs (event_id, user_id, event_type, created_at, metadata)
+        VALUES (%s, %s, %s, %s, %s)
+        """, (event_id, customer.id, event_type, timestamp, metadata))
+
+        session.execute("""
+        INSERT INTO event_logs_by_type (event_id, user_id, event_type, created_at, metadata)
+        VALUES (%s, %s, %s, %s, %s)
+        """, (event_id, customer.id, event_type, timestamp, metadata))
+    except InvalidRequest as error:
+        print(error)
 
 
-def get_daily_events_by_type(event_type: str):
+def get_daily_events_by_type(session: Session, event_type: str):
     """
     Get all events from the database.
+    :param session:
+    :param event_type:
     :return:
     """
-    return session.execute("""SELECT * FROM event_logs WHERE event_type = %s and timestamp > %s  ALLOW FILTERING;""",
-                           (event_type, datetime.now() - timedelta(hours=24),), )
+    try:
+        return session.execute("""SELECT * FROM event_logs_by_type WHERE event_type = %s and created_at > %s;""",
+                               (event_type, datetime.now() - timedelta(hours=24),), )
+    except InvalidRequest as error:
+        print(error)
 
 
-def update_event_metadata(event_id: uuid.UUID, metadata: dict[str, Any]) -> None:
+def update_event_metadata(session: Session, event_id: uuid.UUID, metadata: dict[str, Any]) -> None:
     """
     Update the metadata of the event in the database.
+    :param session:
     :param metadata:
     :param event_id:
     :return:
     """
-    rows = session.execute(
-        "SELECT event_id, timestamp FROM event_logs WHERE event_id = %s AND timestamp < %s",
-        [event_id, datetime.now()],
-    )
-
-    for row in rows:
+    try:
         session.execute(
-            "UPDATE event_logs SET metadata = %s WHERE event_id = %s AND timestamp = %s",
-            [metadata, row.event_id, row.timestamp]
+            """UPDATE event_logs SET metadata = %s WHERE event_id = %s""",
+            [metadata, event_id],
         )
+        event = session.execute("""SELECT event_type, created_at FROM event_logs WHERE event_id = %s""",
+                                [event_id]).one()
+        session.execute(
+            """UPDATE event_logs_by_type SET metadata = %s WHERE created_at = %s AND event_type = %s""",
+            [metadata, event[1], event[0]],
+        )
+    except InvalidRequest | SyntaxException as error:
+        print(error)
+        traceback.print_exc()
 
 
-def delete_old_events() -> None:
+def delete_events_by_week(session: Session, event_type: str) -> None:
     """
     Delete events from the database, that keeps more than one week.
+    :param event_type:
+    :param session:
     :return:
     """
-    rows = session.execute(
-        "SELECT event_id, timestamp FROM event_logs WHERE timestamp < %s ALLOW FILTERING",
-        [datetime.now() - timedelta(weeks=1)],
-    )
+    try:
+        events = session.execute(
+            """SELECT event_id, event_type, created_at FROM event_logs_by_type WHERE event_type = %s AND created_at < %s""",
+            [event_type, datetime.now() - timedelta(weeks=1)]).all()
+        print(events)
 
-    for row in rows:
-        session.execute(
-            "DELETE FROM event_logs WHERE event_id = %s AND timestamp = %s",
-            [row.event_id, row.timestamp]
-        )
+        batch = BatchStatement()
+
+        for event in events:
+            batch.add(
+                "DELETE FROM event_logs WHERE event_id = %s",
+                (event[0],),
+            )
+            batch.add(
+                "DELETE FROM event_logs_by_type WHERE event_type = %s AND created_at = %s",
+                (event[1], event[2]),
+            )
+        session.execute(batch)
+    except InvalidRequest as error:
+        print(error)
+        traceback.print_exc()
--- lesson_12_nosql_databases/cassandra/main.py
+++ lesson_12_nosql_databases/cassandra/main.py
@@ -2,37 +2,29 @@ import uuid
 from datetime import datetime, timedelta
 
 from cassandra_client import create_event_logs_table, insert_event, get_daily_events_by_type, update_event_metadata, \
-    delete_old_events
+    delete_events_by_week, cassandra_session
 from customer import Customer
 
+with cassandra_session() as session:
+    # Create event_logs table
+    create_event_logs_table(session)
 
-def get_events_by_type(event_type: str) -> None:
-    """
-    Return events based on event type
-    :param event_type:
-    :return:
-    """
-    events = get_daily_events_by_type(event_type)
+    customer = Customer(uuid.uuid4(), 'Test User')
+    customer2 = Customer(uuid.uuid4(), 'Test User2')
+    # Login event
+    insert_event(session, customer2, 'login', {'ip': '192.168.1.1', 'device': 'mobile'})
+    insert_event(session, customer2, 'logout', {'ip': '192.168.2.1', 'device': 'mobile'}, datetime.now() - timedelta(days=9))
+    insert_event(session, customer, 'save', {'ip': '192.168.1.1', 'device': 'mobile'})
+
+    # Get daily events with type 'login'
+    events = get_daily_events_by_type(session, "login")
     for event in events:
-        print(f"Event Id: {event.event_id}, Event: {event.event_type}, Time: {event.timestamp}")
+        print(
+            f"Event Id: {event.event_id}, User Id: {event.user_id}, Event: {event.event_type}, Time: {event.created_at}")
 
+    # Update event's metadata
+    event = get_daily_events_by_type(session, 'save')[0]
+    update_event_metadata(session, event.event_id, {"ip": "192.168.120.20", "device": "laptop"})
 
-# Create event_logs table
-create_event_logs_table()
-
-customer = Customer(uuid.uuid4(), 'Test User')
-customer2 = Customer(uuid.uuid4(), 'Test User2')
-# Login event
-insert_event(customer, 'login', {'ip': '192.168.1.1', 'device': 'mobile'})
-insert_event(customer2, 'login', {'ip': '192.168.2.1', 'device': 'mobile'}, datetime.now() - timedelta(weeks=2))
-insert_event(customer, 'save', {'ip': '192.168.1.1', 'device': 'mobile'})
-
-# Get daily events with type 'login'
-get_events_by_type('login')
-
-# Update event's metadata
-event = get_daily_events_by_type('save')[0]
-update_event_metadata(event.event_id, {"ip": "192.168.120.20", "device": "laptop"})
-
-# Delete events
-delete_old_events()
+    # Delete events
+    delete_events_by_week(session, 'logout')
